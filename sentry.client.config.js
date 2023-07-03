bd37185512db02bec5ca39eff955735f22964a67
Abubakar Adeni <78494964+abubakar-adeni@users.noreply.github.com>
2023-07-03 09:37:19 +0700

    Update sentry.client.config.js

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://f15578fbfc0544f7b1aec5ad5644cf71@o4505384071135232.ingest.sentry.io/4505384079917057",
+  dsn: "https://17799da0b13e414cb8d34bca36562a29@o4505384079065088.ingest.sentry.io/4505384082210816",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
